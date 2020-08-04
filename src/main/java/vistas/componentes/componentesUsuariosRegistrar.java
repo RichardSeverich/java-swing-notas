020f98349c012a1dd9b0054fa8d0954c8ba31194
rseveric <rseveric@pros.com>
2020-08-03 23:00:32 -0700

    Add jpicker

--- src/main/java/vistas/componentes/componentesUsuariosRegistrar.java
+++ src/main/java/vistas/componentes/componentesUsuariosRegistrar.java
@@ -7,9 +7,12 @@ import javax.swing.JPasswordField;
 import javax.swing.JTextField;
 import java.util.Properties;
 
-import com.toedter.calendar.JCalendar;
+import org.jdatepicker.impl.UtilDateModel;
+import org.jdatepicker.impl.JDatePanelImpl;
+import org.jdatepicker.impl.JDatePickerImpl;
 
 import vistas.ventanas.VentanaUsuariosRegistrar;
+import vistas.utiles.DateLabelFormatter;
 //import vistas.eventos.EventosLogin;
 
 public class componentesUsuariosRegistrar {
@@ -26,37 +29,43 @@ public class componentesUsuariosRegistrar {
     JLabel labelUsername = new JLabel("Username");
     JTextField fieldUsername = new JTextField();
     labelUsername.setBounds(50, 50, 100, 25);
-    fieldUsername.setBounds(150, 50, 200, 25);
+    fieldUsername.setBounds(200, 50, 200, 25);
     panel.add(labelUsername);
     panel.add(fieldUsername);
     // Password
     JLabel labelContrasena = new JLabel("Contrasena");
     JTextField fieldContrasena = new JTextField();
     labelContrasena.setBounds(50, 100, 100, 25);
-    fieldContrasena.setBounds(150, 100, 200, 25);
+    fieldContrasena.setBounds(200, 100, 200, 25);
     panel.add(labelContrasena);
     panel.add(fieldContrasena);
     // Nombres
     JLabel labelNombres = new JLabel("Nombres");
     JTextField fieldNombres = new JTextField();
     labelNombres.setBounds(50, 150, 100, 25);
-    fieldNombres.setBounds(150, 150, 200, 25);
+    fieldNombres.setBounds(200, 150, 200, 25);
     panel.add(labelNombres);
     panel.add(fieldNombres);
     // Apellidos
     JLabel labelApellidos = new JLabel("Apellidos");
     JTextField fieldApellidos = new JTextField();
     labelApellidos.setBounds(50, 200, 100, 25);
-    fieldApellidos.setBounds(150, 200, 200, 25);
+    fieldApellidos.setBounds(200, 200, 200, 25);
     panel.add(labelApellidos);
     panel.add(fieldApellidos);
     
     // Fecha nacimiento
     JLabel labelFechaNacimiento = new JLabel("Fecha Nacimiento");
-    JCalendar calendar = new JCalendar();        
-    labelFechaNacimiento.setBounds(50, 250, 100, 25);
-    calendar.setBounds(150, 250, 200, 25);
+    UtilDateModel model = new UtilDateModel();
+    Properties properties = new Properties();
+    properties.put("text.today", "Today");
+    properties.put("text.month", "Month");
+    properties.put("text.year", "Year");
+    JDatePanelImpl datePanel = new JDatePanelImpl(model, properties);
+    JDatePickerImpl datePicker = new JDatePickerImpl(datePanel, new DateLabelFormatter());
+    labelFechaNacimiento.setBounds(50, 250, 120, 25);
+    datePicker.setBounds(200, 250, 200, 25);
     panel.add(labelFechaNacimiento);
-    panel.add(calendar);
+    panel.add(datePicker);
   }
 }
